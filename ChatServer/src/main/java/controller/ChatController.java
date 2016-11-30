4c2600d7a6ada931fca73d2266b81e286c02cce0
KFU-user <mrt.shrzdnv@gmail.com>
2016-11-30 11:48:18 +0300

    some fix

--- ChatServer/src/main/java/controller/ChatController.java
+++ ChatServer/src/main/java/controller/ChatController.java
@@ -2,9 +2,13 @@ package controller;
 
 import converter.ConverterDto;
 import dto.ChatDto;
+import dto.MessageDto;
 import model.Chat;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.ChatService;
 import service.UserService;
@@ -24,24 +28,37 @@ public class ChatController {
     UserService userService;
     ConverterDto converter = new ConverterDto();
 
+    // get list of all chats
     @RequestMapping(value = "/chats", method = RequestMethod.GET)
-    public List<ChatDto> getChats(){
+    public ResponseEntity<List<ChatDto>> getChats(){
         List<ChatDto> listChatDto = new ArrayList<>();
         List<Chat> listChat = chatService.findAll();
         for(Chat chat : listChat){
             listChatDto.add(converter.convert(chat));
         }
-        return listChatDto;
+        return new ResponseEntity<List<ChatDto>>(listChatDto, HttpStatus.OK);
     }
+    // create new chat
     @RequestMapping(value = "/chats", method = RequestMethod.POST)
-    public Integer postChats(@RequestBody String chatName, @RequestHeader String token){
+    public ResponseEntity<ChatDto> postChats(@RequestBody String chatName, @RequestHeader String token){
         User user = userService.findByToken(token);
         if (user != null){
             Chat chat = Chat.newBuilder().setName(chatName).setOwnerId(user.getId()).build();
             chatService.save(chat);
-            return chatService.findByName(chatName).getId();
+            return new ResponseEntity<ChatDto>(converter.convert(chatService.findByName(chatName)), HttpStatus.CREATED);
         }else{
-            return null;
+            return new ResponseEntity<ChatDto>(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @RequestMapping(value = "/chats/{chat_id}/messages", method = RequestMethod.GET)
+    public ResponseEntity<List<MessageDto>> getChatsMessages(@RequestHeader String token, @PathVariable("chat_id") String chat_id){
+        User user = userService.findByToken(token);
+        if (user != null){
+
+            return new ResponseEntity<List<MessageDto>>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<List<MessageDto>>(HttpStatus.FORBIDDEN);
         }
     }
 }
